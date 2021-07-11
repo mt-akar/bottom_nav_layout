0d87a78f13576b0f5233a435d4c7d572d25b47d7
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-11 10:56:42 +0300

    export water drop bar

--- lib/bottom_nav_layout.dart
+++ lib/bottom_nav_layout.dart
@@ -1,3 +1,4 @@
 export 'package:bottom_nav_layout/src/layout.dart';
 export 'package:bottom_nav_layout/src/page_stack.dart';
 export 'package:bottom_nav_layout/src/salomon_layout.dart';
+export 'package:bottom_nav_layout/src/water_drop_layout.dart';
