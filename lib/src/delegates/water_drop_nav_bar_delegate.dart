738d43053e14b7d5599fca9554997acd71fb6e9c
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-12 06:11:59 +0300

    water drop bar delegate added

---
+++ lib/src/delegates/water_drop_nav_bar_delegate.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
+
+import '../layout.dart';
+import '../page_stack.dart';
+import 'bar_delegate.dart';
+
+/// This class contains parameters used to create a [WaterDropNavBar] instance, except [WaterDropNavBar.selectedIndex]
+/// which is encapsulated in [BottomNavLayout.pageStack]
+class WaterDropNavBarDelegate extends BarDelegate {
+  WaterDropNavBarDelegate({
+    this.key,
+    required this.barItems,
+    this.onButtonPressed,
+    this.backgroundColor = Colors.white,
+    this.waterDropColor = const Color(0xFF5B75F0),
+    this.iconSize = 30,
+    this.inactiveIconColor,
+  });
+
+  final Key? key;
+  final List<BarItem> barItems;
+  final ValueChanged<int>? onButtonPressed;
+  final Color backgroundColor;
+  final Color waterDropColor;
+  final double iconSize;
+  final Color? inactiveIconColor;
+
+  Widget createBar(PageStack pageStack, void Function(int) onPageSelected) {
+    return WaterDropNavBar(
+        key: key,
+        barItems: barItems,
+        selectedIndex: pageStack.peek(),
+        onButtonPressed: (index) {
+          // Layout functionality
+          onPageSelected(index);
+
+          // Passed in onTap call
+          onButtonPressed?.call(index);
+        },
+        backgroundColor: backgroundColor,
+        waterDropColor: waterDropColor,
+        iconSize: iconSize,
+        inactiveIconColor: inactiveIconColor);
+  }
+}
