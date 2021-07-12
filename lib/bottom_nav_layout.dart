c4b5f13436cbff60b0f4097186a984f83453a8a5
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-12 05:34:07 +0300

    bar delegate exported

--- lib/bottom_nav_layout.dart
+++ lib/bottom_nav_layout.dart
@@ -1,4 +1,5 @@
 export 'package:bottom_nav_layout/src/layout.dart';
+export 'package:bottom_nav_layout/src/bottom_navigation_bar_delegate.dart';
 export 'package:bottom_nav_layout/src/page_stack.dart';
 export 'package:bottom_nav_layout/src/salomon_layout.dart';
 export 'package:bottom_nav_layout/src/water_drop_layout.dart';
