831ef182c530de081d0c2b438b1435727dc0dfea
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-13 05:46:47 +0300

    Update extend_page_stack.dart

--- example/lib/extension_examples/extend_page_stack.dart
+++ example/lib/extension_examples/extend_page_stack.dart
@@ -7,17 +7,20 @@ class MyPageStack extends PageStack {
 
   /// Implement [PageStack.push].
   ///
-  /// Don't forget that, pageStack.peek() is always the current page index in the layout.
-  /// If you want to navigate to a page and peek implementation is "int peek() => last;",
+  /// Never have two of the same item back to back in any part of the stack.
+  /// This causes UI to appear unresponsive when back button is pressed.
+  ///
+  /// Don't forget that, `pageStack.peek()` is always the current page index in the layout.
+  /// If you want to navigate to a page and peek method's implementation is "int peek() => last;",
   /// it is necessary to actually push the index to the end of the stack.
   void push(int pageIndex) {
     // Prevent pushing the last element again,
     // even though the layout never pushes the last element again anyways.
-    // Having same index back to back causes UI to appear unresponsive when back button is pressed.
     if (pageIndex == last) throw Exception("pageIndex pushed cannot be the same as the last index.");
 
-    // For example, push the page both to the start and to the end.
-    addFirst(pageIndex);
+    // For example, reset the stack when the initial index is pushed.
+    // Then push the index.
+    if (pageIndex == first) removeWhere((_) => true);
     addLast(pageIndex);
   }
 
