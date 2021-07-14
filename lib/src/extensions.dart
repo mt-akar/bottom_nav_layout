19a085dde97aa21eb00cb12107055f9e8c3df50b
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-14 21:31:14 +0300

    Update extensions.dart

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -4,14 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// This is an extension class inside bottom_nav_layout package.
-class Nav {
+class NavExtensions {
   static push(BuildContext context, Widget page) {
     var route;
 
-    if (Platform.isAndroid)
-      route = MaterialPageRoute(builder: (_) => page);
-    else
+    if (Platform.isIOS)
       route = CupertinoPageRoute(builder: (_) => page);
+    else
+      route = MaterialPageRoute(builder: (_) => page);
 
     Navigator.push(context, route);
   }
