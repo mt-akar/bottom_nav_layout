1195c17df7be4577df161ec215116031c9241f73
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-14 21:28:19 +0300

    push method improved

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -1,7 +1,18 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Nav{
-  static pushMaterialRoute(BuildContext context, Widget page) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => page));
+/// This is an extension class inside bottom_nav_layout package.
+class Nav {
+  static pushRoute(BuildContext context, Widget page) {
+    var route;
+
+    if (Platform.isAndroid)
+      route = MaterialPageRoute(builder: (_) => page);
+    else
+      route = CupertinoPageRoute(builder: (_) => page);
+
+    Navigator.push(context, route);
   }
 }
