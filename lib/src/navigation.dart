447fe70b3c76700b64156598d2bea87b9a20fa46
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-15 06:51:12 +0300

    Update navigation.dart

--- lib/src/navigation.dart
+++ lib/src/navigation.dart
@@ -3,8 +3,17 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RoutelessNavPage extends StatelessWidget {
-  RoutelessNavPage({required this.navKey, required this.initialPage});
+/// This uses Flutter's standard navigation widget.
+/// You can achieve navigation by pushing and popping widgets without using routes.
+///
+/// Use the following calls inside your page:
+/// Navigator.push(context, MaterialPageRoute(builder: (_) => MyPage()));
+/// Navigator.pop(context);
+/// var canPop = Navigator.canPop(context);
+/// var popped = Navigator.maybePop(context);
+/// Navigator.popUntil(context, (route) => route.isFirst);
+class NamelessNavigator extends StatelessWidget {
+  NamelessNavigator({required this.navKey, required this.initialPage});
 
   final GlobalKey<NavigatorState> navKey;
   final Widget initialPage;
