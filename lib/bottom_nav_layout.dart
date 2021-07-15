954ca7d66b55306e4be16150c700c893cea42593
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-15 06:23:30 +0300

    export nav extensions

--- lib/bottom_nav_layout.dart
+++ lib/bottom_nav_layout.dart
@@ -1,6 +1,7 @@
 export 'package:bottom_nav_layout/src/layout.dart';
-export 'package:bottom_nav_layout/src/page_stack.dart';
 export 'package:bottom_nav_layout/src/nav_bar_delegate.dart';
+export 'package:bottom_nav_layout/src/navigation.dart';
+export 'package:bottom_nav_layout/src/page_stack.dart';
 
 export 'package:bottom_nav_layout/src/delegates/unused/persistent_bottom_nav_bar_delegate.dart';
 export 'package:bottom_nav_layout/src/delegates/convex_bottom_bar_delegate.dart';
--- lib/src/navigation.dart
+++ lib/src/navigation.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RoutelessNavPage extends StatelessWidget {
@@ -14,5 +17,18 @@ class RoutelessNavPage extends StatelessWidget {
         builder: (_) => initialPage,
       ),
     );
+  }
+}
+
+class NavExtensions {
+  static void push(BuildContext context, Widget page) {
+    var route;
+
+    if (Platform.isIOS)
+      route = CupertinoPageRoute(builder: (_) => page);
+    else
+      route = MaterialPageRoute(builder: (_) => page);
+
+    Navigator.push(context, route);
   }
 }
