77b83ab2c36f2c465afae2bf974ddf2e526c792a
mustafa azyoksul <m.azyoksul@gmail.com>
2021-07-15 18:52:10 +0300

    export

--- lib/bottom_nav_layout.dart
+++ lib/bottom_nav_layout.dart
@@ -9,7 +9,7 @@
 // LinkedIn: mustafa-azyoksul      //
 //                                 //
 /////////////////////////////////////
-
+export 'package:bottom_nav_layout/src/animated_indexed_stack.dart';
 export 'package:bottom_nav_layout/src/layout.dart';
 export 'package:bottom_nav_layout/src/navigation.dart';
 export 'package:bottom_nav_layout/src/page_stack.dart';
